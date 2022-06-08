dab4125becb66b671d77fab6f92bccabaf2647b1
Julian Quick <juqu@dtu.dk>
2022-06-08 14:08:35 +0200

    NREL 5 MW RWT initial commit

---
+++ py_wake/examples/data/nrel5mw/__init__.py
@@ -0,0 +1,3 @@
+from py_wake.examples.data.nrel5mw._nrel5mw import (NREL5MW,
+                                                    NREL5MW_RWT,
+                                                    ct_curve, power_curve)
