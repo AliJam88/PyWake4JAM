184221b0b3623b02e9ca819c17540cf8ddb026c7
Julian Quick <juqu@dtu.dk>
2022-06-08 14:46:39 +0200

    pep8 compliance

--- py_wake/examples/data/iea10mw/__init__.py
+++ py_wake/examples/data/iea10mw/__init__.py
@@ -1,3 +1,3 @@
-from py_wake.examples.data.iea10mw._iea10mw import (IEA10MW, 
-                                                    IEA10WM_RWT, 
+from py_wake.examples.data.iea10mw._iea10mw import (IEA10MW,
+                                                    IEA10WM_RWT,
                                                     ct_curve, power_curve)
