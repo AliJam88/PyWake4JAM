f7452559d7a81f36cde5b4cabd3852732181bedd
Julian Quick <juqu@dtu.dk>
2022-06-08 14:25:07 +0200

    IEA 15 MW RWT initial commit

---
+++ py_wake/examples/data/iea15mw/__init__.py
@@ -0,0 +1,3 @@
+from py_wake.examples.data.iea15mw._iea15mw import (IEA15MW,
+                                                    IEA15WM_RWT,
+                                                    ct_curve, power_curve)
