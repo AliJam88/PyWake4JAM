accfdf846474ec6f19b4e80035f3877ff5fb9ad6
Julian Quick <juqu@dtu.dk>
2022-06-16 10:55:06 +0200

    remove superfluous folders in refrence turbine definitions

--- py_wake/examples/data/iea10mw/__init__.py
+++
@@ -1,3 +0,0 @@
-from py_wake.examples.data.iea10mw._iea10mw import (IEA10MW,
-                                                    IEA10WM_RWT,
-                                                    ct_curve, power_curve)
--- py_wake/examples/data/iea15mw/__init__.py
+++
@@ -1,3 +0,0 @@
-from py_wake.examples.data.iea15mw._iea15mw import (IEA15MW,
-                                                    IEA15WM_RWT,
-                                                    ct_curve, power_curve)
