2339c8bd24a34a4b2e448d581dbd529e7ddb7680
AliJam88 <145359401+AliJam88@users.noreply.github.com>
2023-09-21 11:53:42 +0200

    Update __init__.py

--- py_wake/examples/data/AJ_ParqueFicticio/__init__.py
+++ py_wake/examples/data/AJ_ParqueFicticio/__init__.py
@@ -1,6 +1,6 @@
 import os
-# ParqueFicticio_path = os.path.dirname(__file__) + "/"
+ParqueFicticio_path = os.path.dirname(__file__) + "/"
 
-ParqueFicticio_path = os.path.join(os.path.dirname(__file__), 'AJ_ParqueFicticio/')
+#ParqueFicticio_path = os.path.join(os.path.dirname(__file__), 'AJ_ParqueFicticio/')
 
 from ._parque_ficticio import ParqueFicticioSite  # nopep8
